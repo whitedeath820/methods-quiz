889d31c96f5ca882ee47cef4a404f6a1206f8bc8
Sam Allen <allensam0@gmail.com>
2015-09-02 10:21:10 -0600

    passed clsoer to

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -16,8 +16,19 @@ module MethodsQuiz
 	def icy_hot?(a, b)	
 		a > 100 && b < 0 || a < 0 && b > 100
 	end
-	# TODO - write closer_to
 
+def closer_to(tar, ga, gb)
+	m1 = (tar - ga).abs
+	m2 = (tar - gb).abs
+	if m1 > m2
+		return gb
+	elsif m1 < m2
+		return ga
+	else
+		return 0
+	end
+	
+end
 	# TODO - write two_as_one?
 
 	# TODO - write pig_latinify
